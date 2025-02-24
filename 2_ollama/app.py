807239b5d0f8b83171ce83298cbecdb4761bad2c
Adib Waluya <50482736+adibwaluya@users.noreply.github.com>
2025-02-24 21:39:21 +0100

    created simple gen AI app using gemma2 model

---
+++ 2_ollama/app.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.llms import Ollama
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
+
+## Prompt template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant. Please respond to the asked question"),
+        ("user", "Question: {question}")
+    ]
+)
+
+## Streamlit setup
+st.title("Langchain Demo with Gemma:2b")
+input_text = st.text_input("What question do you have in mind?")
+
+## Ollama Llama2 model implementation
+llm = Ollama(model="gemma2:2b")
+output_parser = StrOutputParser()
+chain = prompt | llm | output_parser
+
+if input_text:
+    st.write(chain.invoke({"question": input_text}))
