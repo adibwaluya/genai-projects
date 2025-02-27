53c574441dfe326418d2e4795ffe133433355531
Adib Waluya <50482736+adibwaluya@users.noreply.github.com>
2025-02-27 01:08:45 +0100

    minor fix layout

--- 6_sumap/app.py
+++ 6_sumap/app.py
@@ -9,11 +9,7 @@ st.set_page_config(page_title="LangChain: Summarize Text from YouTube or Website
 st.title("LangChain: Summarize Text from YouTube or Website")
 st.subheader('Summarize URL')
 
-# UI for Groq API key & url/youtube links
-with st.sidebar:
-    groq_api_key = st.text_input("Groq API Key", value = "", type="password")
-
-generic_url = st.text_input("URL", label_visibility="collapsed")
+groq_api_key = st.text_input("Groq API Key", value = "", type="password")
 
 ## Initialize Gemma model
 llm = ChatGroq(model = "Gemma2-9b-It", groq_api_key = groq_api_key)
@@ -26,36 +22,41 @@ Content:{text}
 
 prompt = PromptTemplate(template = prompt_template, input_variables = ["text"])
 
-if st.button("Summarize the content from YouTube or Website"):
+if groq_api_key:
+    
+    generic_url = st.text_input("URL", label_visibility="collapsed")
+    if st.button("Summarize the content from YouTube or Website"):
 
     # validate input
-    if not groq_api_key.strip() or not generic_url.strip():
-        st.error("Please provide the information to get started")
-    elif not validators.url(generic_url):
-        st.error("Please enter a valid URL. It ca be a YouTube or website link")
+        if not groq_api_key.strip() or not generic_url.strip():
+            st.error("Please provide the information to get started")
+        elif not validators.url(generic_url):
+            st.error("Please enter a valid URL. It ca be a YouTube or website link")
 
-    else:
-        try:
-            with st.spinner("Waiting..."):
+        else:
+            try:
+                with st.spinner("Waiting..."):
 
-                # load the site or youtube data
-                if "youtube.com" in generic_url:
-                    loader = YoutubeLoader.from_youtube_url(generic_url, add_video_info = False)
-                else:
-                    # hit the url and server requires headers
-                    loader = UnstructuredURLLoader(
-                        urls = [generic_url],
-                        ssl_verify = False,
-                        headers = {
-                            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
-                        }
-                    )
-                docs = loader.load()
+                    # load the site or youtube data
+                    if "youtube.com" in generic_url:
+                        loader = YoutubeLoader.from_youtube_url(generic_url, add_video_info = False)
+                    else:
+                        # hit the url and server requires headers
+                        loader = UnstructuredURLLoader(
+                            urls = [generic_url],
+                            ssl_verify = False,
+                            headers = {
+                                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+                            }
+                        )
+                    docs = loader.load()
 
-                # Chain for summarization
-                chain = load_summarize_chain(llm, chain_type = "stuff", prompt = prompt)
-                output_summary = chain.run(docs)
+                    # Chain for summarization
+                    chain = load_summarize_chain(llm, chain_type = "stuff", prompt = prompt)
+                    output_summary = chain.run(docs)
 
-                st.success(output_summary)
-        except Exception as e:
-            st.exception(f"Exception:{e}")
+                    st.success(output_summary)
+            except Exception as e:
+                st.exception(f"Exception:{e}")
+else:
+    st.warning("Please enter the Groq API Key")
